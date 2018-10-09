5fb3963a927cca0926ed5ed97692e8a6bbcf692e
jnoortheen <jnoortheen@gmail.com>
2018-10-09 16:03:18 +0530

    feat: add werkzeug debugger

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = ("localhost:3000", "frontend:3000", "127.0.0.1:3000")
+# CORS_ORIGIN_WHITELIST = ("localhost:3000", "frontend:3000", "127.0.0.1:3000", "0.0.0.0:3000",)
 
 ROOT_URLCONF = "backend.urls"
 
